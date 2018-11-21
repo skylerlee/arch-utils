a5ba5a544649dfdae192d24aa5f95f0b0fa6d5a9
skylerlee <skyler.ac.lee@gmail.com>
2018-11-21 23:08:47 +0800

    feat: update file server

--- server/fsserver.go
+++ server/fsserver.go
@@ -1,25 +1,29 @@
 package main
 
 import (
-	"os"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 var (
-	host string = "127.0.0.1"
-	port string = "8080"
+	host string
+	port int
 )
 
 func init() {
+	flag.StringVar(&host, "a", "127.0.0.1", "address to use")
+	flag.IntVar(&port, "p", 8080, "port to bind to")
 }
 
 func main() {
+	flag.Parse()
 	path := os.Args[1]
-	addr := fmt.Sprintf("%s:%s", host, port)
+	addr := fmt.Sprintf("%s:%d", host, port)
 	handler := http.FileServer(http.Dir(path))
 	http.Handle("/", handler)
-	err := http.ListenAndServe(addr, nil)
-	log.Fatal(err)
+	log.Printf("start service at: %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
