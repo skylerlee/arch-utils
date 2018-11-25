84ea1a4372bd5b3fdfd6465371a3ee465ae66b4e
skylerlee <skyler.ac.lee@gmail.com>
2018-11-25 18:41:26 +0800

    feat: add logging

--- server/fsserver.go
+++ server/fsserver.go
@@ -12,16 +12,32 @@ var (
 	port int
 )
 
+type middleware func(http.Handler) http.Handler
+
 func init() {
 	flag.StringVar(&host, "a", "0.0.0.0", "address to use")
 	flag.IntVar(&port, "p", 8080, "port to bind to")
+}
+
+func compose(h http.Handler, mws []middleware) http.Handler {
+	for i := 0; i < len(mws); i++ {
+		h = mws[i](h)
+	}
+	return h
+}
+
+func logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("[%s] %s", r.Method, r.URL)
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
 	flag.Parse()
 	root := flag.Arg(0)
 	addr := fmt.Sprintf("%s:%d", host, port)
-	handler := http.FileServer(http.Dir(root))
+	handler := compose(http.FileServer(http.Dir(root)), []middleware{logRequest})
 	http.Handle("/", handler)
 	log.Printf("start service at: %s", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
