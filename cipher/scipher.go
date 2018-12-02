18eeab6ce29d686a6843a418cbd9f36e0fb1cde6
skylerlee <skyler.ac.lee@gmail.com>
2018-12-03 01:26:54 +0800

    feat: update usage

--- cipher/scipher.go
+++ cipher/scipher.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
 var (
 	password string
-	output string
+	output   string
 )
 
 func init() {
-	flag.StringVar(&password, "p", "", "password to be used")
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage: cipher FILE -p <password> -o <path>")
+		flag.PrintDefaults()
+	}
+	flag.StringVar(&password, "p", "", "password to be applied")
 	flag.StringVar(&output, "o", "", "output file path")
+}
+
+func process(input string) {
+	fmt.Println(input, password, output)
 }
 
 func main() {
 	flag.Parse()
+	if flag.NArg() < 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	input := flag.Arg(0)
+	process(input)
 }
