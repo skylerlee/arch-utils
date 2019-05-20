a183a342904aeb9d2d7261f886e622e3eb5f3566
skylerlee <skyler.ac.lee@gmail.com>
2019-05-21 00:35:59 +0800

    feat: add cli usage

--- _motif/gotif.go
+++ _motif/gotif.go
@@ -1,16 +1,62 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
-func main() {
-	defer func() {
-		err := recover()
-		if err != nil {
-			fmt.Println(err)
+var (
+	help    bool
+	listing bool
+	target  string
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage: gotif [OPTION...]")
+		flag.PrintDefaults()
+	}
+	flag.BoolVar(&help, "h", false, "print help message")
+	flag.BoolVar(&listing, "l", false, "list gist files")
+	flag.StringVar(&target, "o", "", "open a gist file")
+}
+
+// UsageError means there is a problem with command usage
+type UsageError struct {
+	message string
+}
+
+// NewUsageError creates an UsageError with a message
+func NewUsageError(message string) *UsageError {
+	return &UsageError{message}
+}
+
+func (e *UsageError) Error() string {
+	return e.message
+}
+
+func handleError() {
+	if err := recover(); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		switch err.(type) {
+		case *UsageError:
+			flag.Usage()
 		}
-	}()
+		os.Exit(1)
+	}
+}
+
+func main() {
+	defer handleError()
+	flag.Parse()
+	if flag.NFlag() < 1 {
+		panic(NewUsageError("not enough arguments"))
+	}
+	if help {
+		flag.Usage()
+		return
+	}
 	conf := LoadConf("conf/gist.json")
 	client := Client{}
 	// client.Filter = func(req *http.Request) *http.Request {
