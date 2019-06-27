cfe2451e27f7326728b88187da11cbd83ffb5611
skylerlee <skyler.ac.lee@gmail.com>
2019-06-28 01:46:36 +0800

    feat: add ssrs

---
+++ _proxy/ssrs.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+def fetch_feed(url):
+    pass
+
+
+def parse_feed(url):
+    fetch_feed(url)
+
+
+def main():
+    parse_feed(sys.argv[1])
+
+
+if __name__ == '__main__':
+    main()
