4805288ce9ac82fe28a8df55bc66591066aa2b52
skylerlee <skyler.ac.lee@gmail.com>
2019-06-28 03:31:44 +0800

    feat: update

--- _proxy/ssrs.py
+++ _proxy/ssrs.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import base64
+from urllib import request
 
 
 def fetch_feed(url):
-    pass
+    resp = request.urlopen(url)
+    return resp.read()
+
+
+def padding(data):
+    length = len(data)
+    pad = 4 - length % 4
+    return data + b'=' * pad
 
 
 def parse_feed(url):
-    fetch_feed(url)
+    raw = fetch_feed(url)
+    data = padding(raw)
+    text = base64.decodebytes(data).decode('utf-8')
+    lines = text.split('\n')
+    print(lines)
 
 
 def main():
