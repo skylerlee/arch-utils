56a8d870846632ea39bbd0f6974921feb47ac55f
skylerlee <skyler.ac.lee@gmail.com>
2019-06-28 23:03:19 +0800

    feat: update

--- _proxy/ssrs.py
+++ _proxy/ssrs.py
@@ -9,24 +9,25 @@ def fetch_feed(url):
     resp = request.urlopen(url)
     return resp.read()
 
-
 def padding(data):
     length = len(data)
     pad = 4 - length % 4
     return data + b'=' * pad
 
+def get_lines(s):
+    lines = s.split('\n')
+    return filter(lambda i: i, lines)
+
+def parse_ssr_url(url):
+    pass
 
 def parse_feed(url):
     raw = fetch_feed(url)
     data = padding(raw)
     text = base64.decodebytes(data).decode('utf-8')
-    lines = text.split('\n')
-    print(lines)
-
-
-def main():
-    parse_feed(sys.argv[1])
+    for line in get_lines(text):
+        parse_ssr_url(line)
 
 
 if __name__ == '__main__':
-    main()
+    parse_feed(sys.argv[1])
