33319eba93d779505e9fce846737e7a572044c6a
skylerlee <skyler.ac.lee@gmail.com>
2020-02-01 01:27:02 +0800

    feat: update

--- _proxy/ssrs.py
+++ _proxy/ssrs.py
@@ -2,7 +2,7 @@
 
 import sys
 import base64
-from urllib import request
+from urllib import request, parse
 
 
 def fetch_feed(url):
@@ -18,6 +18,22 @@ def get_lines(s):
     lines = s.split('\n')
     return filter(lambda i: i, lines)
 
+def process_enc(s):
+    result = parse.urlparse(s)
+    path = result.path
+    data = padding(path.encode('utf-8'))
+    path2 = base64.b64decode(data, '-_').decode('utf-8')
+    params = parse.parse_qsl(result.query)
+    params2 = []
+    for key, val in params:
+        data = padding(val.encode('utf-8'))
+        val2 = base64.b64decode(data, '-_').decode('utf-8')
+        params2.append((key, val2))
+    return {
+        "path": path2,
+        "params": params2,
+    }
+
 def parse_ssr_url(url):
     protocol = 'ssr://'
     if not url.startswith(protocol):
@@ -25,11 +41,13 @@ def parse_ssr_url(url):
     raw = url[len(protocol):]
     data = padding(raw.encode('utf-8'))
     text = base64.b64decode(data, '-_').decode('utf-8')
-    return text.split(':')
+    segs = text.split(':')
+    segs[-1] = process_enc(segs[-1])
+    return segs
 
 def parse_feed(url):
-    raw = fetch_feed(url)
-    data = padding(raw)
+    feed = fetch_feed(url)
+    data = padding(feed)
     text = base64.decodebytes(data).decode('utf-8')
     for line in get_lines(text):
         segs = parse_ssr_url(line)
